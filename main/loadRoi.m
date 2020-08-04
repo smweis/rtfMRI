0f10de7c7e24ae0c99dbe3dbef101a61f557f9e1
Weisberg <stevenweisberg@ufl.edu>
2020-08-04 14:20:36 -0400

    replace load_untouch_nii with matlab function readnifti

--- main/loadRoi.m
+++ main/loadRoi.m
@@ -1,5 +1,5 @@
 function ROIIndex = loadRoi(roiPath)
 % Load the NIFTI located at roiPath, and turn it into a logical index
-roiNifti = load_untouch_nii(roiPath);
+roiNifti = niftiread(roiPath);
 ROIIndex = roiNifti.img;
 ROIIndex = logical(ROIIndex);
