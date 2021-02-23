9a5ea64e66e5707ec936e318790fc584e33eb29c
Frank <jacob.frank@ufl.edu>
2021-02-23 13:26:06 -0500

    Edited getPaths

--- main/getPaths.m
+++ main/getPaths.m
@@ -2,11 +2,11 @@ function [subjectPath, scannerPath, codePath, scratchPath] = getPaths(subject,pr
 % Get all relevant paths for experiment
 
 % Path where new DICOMs will come in.
-scannerPath = getpref(projectName','scannerBasePath');
+scannerPath = getpref(projectName,'scannerBasePath');
 
 % Path to subject-relevant files (like their MPRAGE and scout images)
 subjectPath = getpref(projectName, 'currentSubjectBasePath');
-subjectPath = [subjectPath filesep subject];
+subjectPath = [subjectPath filesep filesep 'derivatives' filesep 'realTime' filesep subject];
 
 % Path to the scratch directory for saving NIFTIs temporarily and locally.
 scratchPath = getpref(projectName, 'analysisScratchDir');
@@ -14,6 +14,6 @@ scratchPath = getpref(projectName, 'analysisScratchDir');
 % Path to the repository.
 projectRootPath = getpref(projectName,'projectRootDir');
 
-codePath = fullfile(projectRootPath','..','toolboxes','rtfmri');
+codePath = fullfile(projectRootPath,'..','toolboxes','rtfmri');
 
 end
